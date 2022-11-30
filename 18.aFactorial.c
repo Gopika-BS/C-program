7c48d408016b0d602e316257baa30005509f4ba9
unknown <amal.nandan@skizzle.tech>
2022-11-30 11:46:16 +0530

    Multiplication Table

--- 18.aFactorial.c
+++ 18.aFactorial.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main (void){
-    int num,fact=1,i,factorial;
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int num, fact = 1, i, factorial;
     printf("Enter any number to find its factorial\n");
-    scanf("%d",&num);
-    for(i = 1;i < fact; i++){
+    scanf("%d", &num);
+    for (i = 1; i < fact; i++)
+    {
         factorial = fact * i;
     }
-    printf("'%d!' is - %d\n",num,factorial);
-
+    printf("'%d!' is - %d\n", num, factorial);
     return EXIT_SUCCESS;
 }
