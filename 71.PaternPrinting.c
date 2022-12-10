672a9987c1badfb291f25dc760f8284a7d48118c
Gopika B.S <letters.to.gopika@gmail.com>
2022-12-10 13:58:02 +0530

    C program for patern printing

---
+++ 71.PaternPrinting.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (void){
+    int limit=10,i,j;
+    for(i=1;i<limit;i++){
+        for(j=0;j<i;j++){
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    return EXIT_SUCCESS;
+}
---
+++ 71.a.PaternPrinting copy.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (void){
+    int limit,i,j;
+    printf("Enter the number of limit of patern you want\n");
+    scanf("%d",&limit);
+    for(i=1;i<limit;i++){
+        for(j=0;j<i;j++){
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    return EXIT_SUCCESS;
+}
